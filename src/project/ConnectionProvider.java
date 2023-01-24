862b651a06ad8f3b9a291d0498d5b9a8d75ef4ab
Satyendra Kumar <98349239+ssatyendrak13@users.noreply.github.com>
2023-01-24 18:34:00 +0530

    initial commit

---
+++ src/project/ConnectionProvider.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+import java.sql.*;
+/**
+ *
+ * @author PC
+ */
+public class ConnectionProvider {
+    
+    public static Connection getCon(){
+        
+        
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bms" , "root" , "Root@123");
+            return con;
+        }catch(Exception e){
+            return null;
+        }
+    }
+    
+}
